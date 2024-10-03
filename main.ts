bb18a4d310fd6d03b609b560fb444c20db99b4f6
bhavikdodda <bhavikdodda@gmail.com>
2024-10-03 17:58:38 +0530

    settings page

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-import { Editor, Plugin } from 'obsidian'
+import { Plugin } from 'obsidian'
 
-export default class ExamplePlugin extends Plugin {
+import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from "./settings"
+
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
 	async onload() {
+		await this.loadSettings();
+
+
 		console.log("Bhavik's Plugin Loaded");
 		this.app.workspace.on('active-leaf-change',async ()=>{
 			const file = this.app.workspace.getActiveFile();
@@ -16,13 +24,25 @@ export default class ExamplePlugin extends Plugin {
 							var newcontent=content.replace(/formatDateToToday\(\"[0-9a-zA-Z ]+\"\)\;/,`formatDateToToday("${dateText}");`);
 							newcontent=newcontent.replace(/##### Done on [0-9a-zA-Z ]+\n/,`##### Done on ${dateText}\n`);
 							await this.app.vault.modify(TasksDone,newcontent);
-							console.log("done updating the dataview in the Tasks done note")
+							console.log("done updating the dataview in the Tasks done note");
 						}
 					}
 				}
 			}
 			
 		})
-		
+		//
+		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
+	//
+	
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		console.log(this.settings)
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+	
 }
---
+++ settings.ts
@@ -0,0 +1,36 @@
+import { App, PluginSettingTab, Setting } from 'obsidian'
+import type MyPlugin from "./main";
+
+export interface MyPluginSettings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default2'
+}
+
+export class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
